38428a8a96dbae3b6d1e1155666cea77cec69c90
Tristan B. V. Kildaire <21629986@sun.ac.za>
2022-03-05 19:53:24 +0200

    Added explicit column major

---
+++ lists/l3/explicit_cm_lr.c
@@ -0,0 +1,37 @@
+#include<assert.h>
+
+int count = 0;
+
+int counter()
+{
+	count++;
+	return count;
+}
+
+
+int main()
+{
+	int c1 = counter();
+	int c2 = counter();
+	int c3 = counter();
+	int c4 = counter();
+	int c5 = counter();
+	int c6 = counter();
+
+	int arrayOfArrays[3][2] = {
+		{c1, c4},
+		{c2, c5},
+		{c3, c6}
+	};
+
+	assert(arrayOfArrays[0][0] == 1);
+	assert(arrayOfArrays[0][1] == 4);
+
+	assert(arrayOfArrays[1][0] == 2);
+	assert(arrayOfArrays[1][1] == 5);
+
+	assert(arrayOfArrays[2][0] == 3);
+	assert(arrayOfArrays[2][1] == 6);
+
+	return 0;
+}

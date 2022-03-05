438128b9cdfda7984f913239fef2eef91fb82fc3
Tristan B. V. Kildaire <21629986@sun.ac.za>
2022-03-05 19:43:50 +0200

    Formatted explicit example

--- lists/l3/explicit_rm_lr.c
+++ lists/l3/explicit_rm_lr.c
@@ -11,13 +11,13 @@ int counter()
 
 int main()
 {
-  int c1 = counter();
-  int c2 = counter();
-  int c3 = counter();
-  int c4 = counter();
-  int c5 = counter();
-  int c6 = counter();
-  
+	int c1 = counter();
+	int c2 = counter();
+	int c3 = counter();
+	int c4 = counter();
+	int c5 = counter();
+	int c6 = counter();
+
 	int arrayOfArrays[3][2] = {
 		{c1, c2},
 		{c3, c4},
@@ -29,9 +29,9 @@ int main()
 
 	assert(arrayOfArrays[1][0] == 3);
 	assert(arrayOfArrays[1][1] == 4);
-	
+
 	assert(arrayOfArrays[2][0] == 5);
 	assert(arrayOfArrays[2][1] == 6);
-	
+
 	return 0;
 }

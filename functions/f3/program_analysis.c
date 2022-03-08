e3f524c2a22ca486c00e99d7ee0de82547e1a793
Tristan B. Kildaire <21629986@sun.ac.za>
2022-03-08 15:49:06 +0200

    Create program_analysis.c

---
+++ functions/f3/program_analysis.c
@@ -0,0 +1,22 @@
+#include<assert.h>
+#include<stdio.h>
+
+int global = 0;
+
+int f(int x1)
+{
+	global = x1;
+	return 0;
+}
+
+int main()
+{
+	int result = f(1)+f(2);
+
+	/**
+	* left to right
+	*/
+  assert(global == 2);
+	
+	return 0;
+}

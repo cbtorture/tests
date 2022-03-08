424ad5520a340b8023c9d0626adfbff18b53ef0c
Tristan B. Kildaire <21629986@sun.ac.za>
2022-03-08 13:40:46 +0200

    Create explicit_rl.c

---
+++ lists/l2/explicit_rl.c
@@ -0,0 +1,26 @@
+
+#include<assert.h>
+#include<stdio.h>
+
+/* Global data */
+int count = 0;
+
+/* Counting function */
+int counter()
+{
+	count++;
+	return count;
+}
+
+int main()
+{
+	int res1_val = counter();
+	int res2_val = counter();
+  int res3_val = counter();
+  
+	int result[3] = {res3_val, res2_val, res1_val};
+
+	assert(result[0] == 3);
+	assert(result[1] == 2);
+  assert(result[2] == 1);
+}

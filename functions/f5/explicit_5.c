a182b31a888fa9a82d7cc140bb546c098d09276c
Tristan B. Kildaire <21629986@sun.ac.za>
2022-03-10 10:34:01 +0200

    Update explicit_5.c

--- functions/f5/explicit_5.c
+++ functions/f5/explicit_5.c
@@ -9,6 +9,12 @@ int counter()
 	return globalCounter;
 }
 
+int counter2()
+{
+	globalCounter = globalCounter + 1;
+	return globalCounter;
+}
+
 int f(int x1, int x2)
 {
 	return (x1*2)+x2;
@@ -16,9 +22,11 @@ int f(int x1, int x2)
 
 int main()
 {
-	int arg2 = counter();
-	int arg1 = counter();
-	int result = f(arg1, arg2);
+	int result2 = counter2();
+	int result1 = counter();
+	int result = f(result2, result1);
 
 	assert(result == 5);
+
+	return 0;
 }

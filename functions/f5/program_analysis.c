309ba2312b436df4fdf168d77a0da1272777de7c
Tristan B. Kildaire <deavmi@redxen.eu>
2022-03-17 11:53:55 +0200

    Fixed comma test

--- functions/f5/program_analysis.c
+++ functions/f5/program_analysis.c
@@ -1,17 +1,7 @@
 #include<assert.h>
-#include<stdio.h>
-
-int f(int x1, int x2)
-{
-	return (x1*2)+x2;
-}
-
 
 int map[3] = {0,0,0};
-
 int x = 0;
-
-
 int id = 0;
 
 int updateX(int sig)
@@ -25,17 +15,13 @@ int updateX(int sig)
 
 int main()
 {
-		//int result = (counter(), f)(1,1);
+		int result = (updateX(1), updateX(2), updateX(3));
 
-		/* 1, f(2, 3) */
-		/* 1, f(3, 2) */
-		int result = (updateX(1), f)(updateX(2), updateX(3));
-
-		/* 1, f(2, 3) */
+		/* (1, 2, 3) and returning the last, 3 */
 		assert(map[0] == 1);
 		assert(map[1] == 2);
 		assert(map[2] == 3);
-		assert(result == 7);		
+		assert(result == 3);		
 
 		return 0;
 }

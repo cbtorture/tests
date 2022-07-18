74838195fdbc7ec3043fba36b0fccec160fc6df7
Tristan B. V. Kildaire <21629986@sun.ac.za>
2022-07-18 16:09:42 +0200

    WIP: Binary operator tests

---
+++ binop/bop1/test_lr.c
@@ -0,0 +1,28 @@
+#include<assert.h>
+
+int counter  = 0;
+
+int arr[2] = {0,0};
+
+int count(int i)
+{
+	counter = counter + 1;
+	arr[i-1] = counter;
+	
+	return 0;
+}
+
+int main()
+{
+	int j = count(1) + count(2);
+
+
+	assert(arr[0] == 1);
+	assert(arr[1] == 2);
+	
+	j++;
+
+	
+
+	return 0;
+}
